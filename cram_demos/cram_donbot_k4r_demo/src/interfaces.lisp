be5605858d39932b8d21eeab8e84ece1b2808950
HaSu <hawkin@uni-bremen.de>
2020-09-22 20:59:11 +0200

    [k4r-demo] added service-call for GET-TARGET-POSE service. fixed typos

--- cram_demos/cram_donbot_k4r_demo/src/interfaces.lisp
+++ cram_demos/cram_donbot_k4r_demo/src/interfaces.lisp
@@ -3,7 +3,13 @@
 (in-package :k4r)
 
 (defun call-send-image-service (name)
-  "pokes the send-image client to take and send an image"
+  "pokes the send-image service to take and send an image"
   (if (not (roslisp:wait-for-service "send_image" 10))
       (roslisp:ros-warn (send-image-client) "timed out waiting for send-image service")
       (roslisp:call-service "send_image" 'k4r_common_srvs-srv:SendImage :name name)))
+
+(defun call-get-target-pose-service (object-ID)
+  "pokes the get-target-psoe service to take and send an image"
+  (if (not (roslisp:wait-for-service "get_target_pose" 10))
+      (roslisp:ros-warn (get-target-pose-client) "timed out waiting for get_target_pose service")
+      (roslisp:call-service "get_target_pose" 'k4r_common_srvs-srv:GetTargetPose :object object-ID)))
