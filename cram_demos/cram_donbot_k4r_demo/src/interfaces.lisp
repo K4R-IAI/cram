8e19d66a892715f45797cfe997531ecb58ba3948
HaSu <hawkin@uni-bremen.de>
2020-09-22 21:04:39 +0200

    [k4r-demo] added GET-MAP service call. fixed typos

--- cram_demos/cram_donbot_k4r_demo/src/interfaces.lisp
+++ cram_demos/cram_donbot_k4r_demo/src/interfaces.lisp
@@ -9,7 +9,14 @@
       (roslisp:call-service "send_image" 'k4r_common_srvs-srv:SendImage :name name)))
 
 (defun call-get-target-pose-service (object-ID)
-  "pokes the get-target-psoe service to take and send an image"
+  "pokes the get-target-pose service to get a pose to go to"
   (if (not (roslisp:wait-for-service "get_target_pose" 10))
       (roslisp:ros-warn (get-target-pose-client) "timed out waiting for get_target_pose service")
       (roslisp:call-service "get_target_pose" 'k4r_common_srvs-srv:GetTargetPose :object object-ID)))
+
+(defun call-get-map-service ()
+  "pokes the get-map service to get the map from the platform"
+  (if (not (roslisp:wait-for-service "get_map" 10))
+      (roslisp:ros-warn (get-map) "timed out waiting for get_map service")
+      (roslisp:call-service "get_map" 'k4r_common_srvs-srv:GetMap)))
+
